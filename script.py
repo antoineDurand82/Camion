3f2820f67c526abf62e4024970acab84f6242fac
Antoine.D <antoine.durand@ynov.com>
2019-10-24 03:49:04 +0200

    fin pause

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 from dictionnaire import villes
-import math
+from math import *
+import time
 
 
 
@@ -8,16 +9,41 @@ def livraison(depart, arrivee):
     distance_trajet = villes[depart][arrivee] * 1000
     distance_parcourue = 0
     temps_s = 0
-    while (distance_parcourue != distance_trajet):
-        while (vitesse_m != 90000):
-            vitesse_m += 10000
-            distance_parcourue += vitesse_m / 3600
-            temps_s += distance_parcourue / (vitesse_m / 3600)
+    t_avant_pause = 0
+    pause = False
+    nb_Pause = 0
+    while (distance_parcourue < distance_trajet):
+
+        if (t_avant_pause == 6660):
+            pause = True
             
-        distance_parcourue += vitesse_m / 3600
-        temps_s += distance_parcourue / (vitesse_m / 3600)
-        print(distance_parcourue)
-        print(temps_s / 60)
+
+        if (pause == True):
+            if (vitesse_m > 25/9):
+                vitesse_m -= 25 / 9
+                temps_s += 60
+                distance_parcourue = vitesse_m * temps_s
+            elif (vitesse_m < 25/9):
+                temps_s += 900
+                t_avant_pause = 0
+                vitesse_m = 0
+                pause = False
+                nb_Pause += 1
+        else:
+            temps_s += 60
+            t_avant_pause += 60
+            distance_parcourue = vitesse_m * temps_s
+
+        if (t_avant_pause < 600):
+            if (vitesse_m < 25):
+                vitesse_m += 25 / 9
+                distance_parcourue = vitesse_m * temps_s
+
+        print(t_avant_pause)
+        print("Vitesse = " + str(vitesse_m))
+        print("Distance = " + str(distance_parcourue))
+        print("Temps = " + str(temps_s))
+        print("Nombre de pause total = " + str(nb_Pause))
       
         
         
